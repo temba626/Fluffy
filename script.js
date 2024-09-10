90c782c2852f3fc9110321f62909b3198e98d932
Brandon Temba <tembzz@Tembzz.tembzz.com>
2024-09-10 22:20:41 +0300

    script js details added

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Toggle Navbar */
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
