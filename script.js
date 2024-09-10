596cc6bc07d2ff0e4ebfa75d4c6369596a7c1dea
Brandon Temba <tembzz@Tembzz.tembzz.com>
2024-09-10 22:53:09 +0300

    script js details added

--- script.js
+++ script.js
@@ -36,3 +36,16 @@ header.classList.toggle('sticky', window.scrollY > 100);
 menuIcon.classList.remove('fa-xmark');
 navbar.classList.remove('active');
 }; 
+
+/* Remove Toggle icon and Navbar */
+ScrollReveal ({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
