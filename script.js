09a8035e51cb65cc20a6bfc3b4978ea657d1e4bb
Brandon Temba <tembzz@Tembzz.tembzz.com>
2024-09-10 22:29:50 +0300

    script js details added

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active')
 }
+
+/* Scroll section */
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /* Toggle Navbar */
+
+}
